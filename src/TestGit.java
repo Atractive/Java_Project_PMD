6bfc7f008b18eb4b339e68e6260c409ce38005e4
Matthias Ambroise <mambrois@debian.univ-jfc.fr>
2018-03-20 11:41:52 +0100

    Test de Git

---
+++ src/TestGit.java
@@ -0,0 +1,11 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		/* Fichier de test utilisé pour vérifier
+		 * le bon fonctionnement de Git
+		 */
+		System.out.println("Coucou Git");
+	}
+
+}
