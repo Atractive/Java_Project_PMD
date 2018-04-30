ab633c04ba690c18ac7712200405cf992538397f
Diegoortizz <ortiz.diego81@gmail.com>
2018-05-01 00:00:13 +0200

    Test debug

--- src/PROJET_BROUILLON/Main.java
+++ src/PROJET_BROUILLON/Main.java
@@ -1,22 +1,13 @@
 package PROJET_BROUILLON;
 
-import java.awt.Button;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
 public class Main extends Application {
 
@@ -25,7 +16,6 @@ public class Main extends Application {
 	public static File[] AllImages;
 
 	public void start(Stage stage) throws Exception {
-
 		ModeleTest modele = new ModeleTest(ImageDirectory);
 		FXMLLoader firstLoader = new FXMLLoader(getClass().getResource("GalleryView.fxml"));
 		firstLoader.setController(new ControlerMDI(modele));
@@ -47,7 +37,6 @@ public class Main extends Application {
 				e1.printStackTrace();
 			}
 		});
-
 
 	}
 
